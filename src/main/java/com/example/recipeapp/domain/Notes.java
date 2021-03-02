e79e270575f20639da56ecfad0cebce2bc09d0bf
Mariah <mariah.zammit.18@um.edu.mt>
2021-03-02 14:23:26 +0100

    Excluding Properties for Hash and Equals

--- src/main/java/com/example/recipeapp/domain/Notes.java
+++ src/main/java/com/example/recipeapp/domain/Notes.java
@@ -1,10 +1,12 @@
 package com.example.recipeapp.domain;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import javax.persistence.*;
 
 @Data
+@EqualsAndHashCode(exclude = {"recipe"})
 @Entity
 public class Notes {
 
--- src/main/java/com/example/recipeapp/domain/Recipe.java
+++ src/main/java/com/example/recipeapp/domain/Recipe.java
@@ -1,12 +1,14 @@
 package com.example.recipeapp.domain;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
 @Data
+@EqualsAndHashCode(exclude = {"notes","ingredients"})
 @Entity
 public class Recipe {
 
