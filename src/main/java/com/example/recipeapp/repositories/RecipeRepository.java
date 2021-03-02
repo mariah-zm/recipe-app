6618e0c4d449f80c081f584824013cabea08dc3b
Mariah <mariah.zammit.18@um.edu.mt>
2021-03-02 09:33:23 +0100

    Spring Data JPA query methods

--- src/main/java/com/example/recipeapp/repositories/RecipeRepository.java
+++ src/main/java/com/example/recipeapp/repositories/RecipeRepository.java
@@ -3,5 +3,10 @@ package com.example.recipeapp.repositories;
 import com.example.recipeapp.domain.Recipe;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface RecipeRepository extends CrudRepository<Recipe, Long> {
+
+    Optional<Recipe> findByDescription(String description);
+
 }
